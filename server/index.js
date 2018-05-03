426884b38d6dc3afd718d1f897bd648b6a3411d7
LexSwed <oleksandr.shvechykov@pipedrive.com>
2018-05-03 20:11:35 +0300

    points

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
-const mockData = [{ lat: 50.577171, lng: 20.435938 }, { lat: 50.537171, lng: 20.465938 }];
+const mockData = [{ lat: 50.577171, lng: 20.435938 }, { lat: 51.437171, lng: 19.465938 }];
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 
