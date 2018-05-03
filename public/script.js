1b044fab3317f87f59970fa05c2186d2ccaaa3f8
KGKallasmaa <karl.gustav1789@gmail.com>
2018-05-03 20:13:59 +0300

    -

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
 let map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('google-map'), {
-		center: { lat: -34.397, lng: 150.644 },
-		zoom: 8
+        center: new google.maps.LatLng(-25.763, 132.044),
+        zoom: 8,
+        minZoom: 1
 	});
+    var myLatLng = {lat: -25.363, lng: 131.044};
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'Test point!'
+    });
+
 }
+
+
--- server/index.js
+++ server/index.js
@@ -1,8 +1,52 @@
 const express = require('express');
+var bodyParser = require("body-parser");
 const app = express();
+
 
 app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+var server = app.listen(8080, function() {
+    console.log('Ready on port %d', server.address().port);
+});
+
+
+var obj = {
+    "nm_questionario":{"isEmpty":"MSGE1 - Nome do Question&aacute;rio"},
+    "ds_questionario":{"isEmpty":"MSGE1 - Descri&ccedil;&atilde;o do Question&aacute;rio"},
+    "dt_inicio_vigencia":{"isEmpty":"MSGE1 - Data de Vig&ecirc;ncia"}
+};
+console.log(Object.keys(obj));
+
+const key = 'deals';
+var deals = {};
+deals[key] = [];
+
+    app.use(bodyParser())
+        .post('/adddeal', function(req, res) {
+            //  console.log(req.body) // populated!
+            console.log(req.body.current.currency)
+            var lat = req.body.current["7b138ae55693cdfda5bf88c8b8c8de7df332a3f8_lat"];
+            var long = req.body.current["7b138ae55693cdfda5bf88c8b8c8de7df332a3f8_long"];
+            var status = req.body.current["status"];
+            var user_id = req.body.current["user_id"];
+
+            if (status == 'won'){
+                var data = {
+                    user_id: user_id,
+                    status: status,
+                    lat:lat,
+                    long:long
+                };
+                deals[key].push(data);
+            }
+
+
+            console.log(deals);
+            res.end('OK');
+        })
+        .listen(PORT,() => console.log("test"));
+
+
+console.log(deals);
