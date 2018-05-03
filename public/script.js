7c652cbcc825c4cd9f2b0afe1ed8b5ed452a4437
LexSwed <oleksandr.shvechykov@pipedrive.com>
2018-05-03 20:10:13 +0300

    frontend stuff

--- public/script.js
+++ public/script.js
@@ -2,6 +2,31 @@ let map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('google-map'), {
 		center: { lat: -34.397, lng: 150.644 },
-		zoom: 8
+		zoom: 2,
+		mapTypeId: 'satellite'
 	});
+	fetch('/deals')
+		.then(res => res.json())
+		.then(createMarkers);
+}
+
+function createMarkers(markers) {
+	markers.map(el => {
+		const marker = new google.maps.Marker({
+			position: el,
+			map: map,
+			title: 'coordinates',
+			animation: google.maps.Animation.DROP
+		});
+		marker.addListener('click', () => toggleBounce(marker));
+		return marker;
+	});
+}
+
+function toggleBounce(marker) {
+	if (marker.getAnimation() !== null) {
+		marker.setAnimation(null);
+	} else {
+		marker.setAnimation(google.maps.Animation.BOUNCE);
+	}
 }
--- server/index.js
+++ server/index.js
@@ -5,4 +5,10 @@ app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3000;
 
+const mockData = [{ lat: 50.577171, lng: 20.435938 }, { lat: 50.537171, lng: 20.465938 }];
+
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+
+app.get('/deals', (req, res) => {
+	res.send(mockData);
+});
