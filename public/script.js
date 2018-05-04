f6672a4c0ba64c63db81e02908674bbd076f9ed0
KGKallasmaa <karl.gustav1789@gmail.com>
2018-05-04 14:21:30 +0300

    ----

--- public/script.js
+++ public/script.js
@@ -5,10 +5,14 @@ const markers = [{lat: 50.5689, lng: 60.4565}, {lat: 51.5689, lng: 61.4565}];
 var max = Number.MIN_SAFE_INTEGER;
 var min = Number.MAX_SAFE_INTEGER;
 
+var color_dict = {};
+var colors = ['#F00', '#000000', '#808080','#800000','#FFFC33','#7FFF33','#33FFA4','#338AFF','#5933FF'];
+
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('google-map'), {
 		center: { lat: 43, lng:25},
-		zoom: 8,
+		zoom: 6,
 	});
 
     if (navigator.geolocation) {
@@ -19,11 +23,9 @@ function initMap() {
     }
 
 	fetch('/deals')
-    setInterval(function() {
-        fetch('/deals')
 		.then(res => res.json())
-		.then(createMarkers)
-    }, 1000);
+		.then(createMarkers);
+		//createMarkers(markers);
 }
 
 
@@ -43,12 +45,18 @@ function createMarkers(markers) {
 	markers.deals.map(el => {
 		var pos = { lat: el.lat, lng: el.long }
 
-		if (el.value < min){
-		    min = el.value;
+		if (el.value < min){min = el.value;}
+        else if (el.value > max){max = el.value;}
+
+        if (colors.size != 0){
+		    var color = colors[Math.floor(Math.random()*colors.length)];
+            color_dict[el.user_id] = color;
+            var index = colors.indexOf(color);
+            if (index > -1) {
+                colors.splice(index, 1);
+            }
         }
-        else if (el.value > max){
-		    max = el.value;
-        }
+
 
 
 		var size = size_f(el.value);
@@ -60,7 +68,7 @@ function createMarkers(markers) {
 			icon: {
 				path: google.maps.SymbolPath.CIRCLE,
 				scale: size,
-				fillColor: '#F00',
+				fillColor:color_dict[el.user_id],
 				fillOpacity: 1,
 				strokeWeight: 0.2
 			}
@@ -90,10 +98,12 @@ function size_f(value) {
 
 }
 
+
 function toggleBounce(marker) {
 	if (marker.getAnimation() !== null) {
 		marker.setAnimation(null);
 	} else {
 		marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function(){ marker.setAnimation(null); }, 750*10);
 	}
 }
